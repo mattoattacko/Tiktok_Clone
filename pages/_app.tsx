94e13ed454cb551c9a15b00ba76cd94dd1bd2c79
unknown <matt.mcquain@gmail.com>
2022-07-12 18:36:42 -0700

    started adding components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,40 @@
 //where we create the template structure for our files and folders
+//a component is a specific page that we can render in our app. Some of them need to be shared across multiple pages (eg: Navbar)
+//92vh is 92% of the height of the screen
 
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import '../styles/globals.css'
+
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+
+  const [isSSR, setIsSSR] = useState(true)
+
+  //if we make it to the useEffect, that means our code is being executed inside of React, which is client side. So we setIsSSR to false because its not server side.
+  useEffect(() => {
+    setIsSSR(false)
+  }, [])
+
+  if(isSSR) return null;//if we are server side rendering, then we don't want to show our components. 
+
+  return (
+    <div>
+      <Navbar />
+      <div className='flex gap-6 md:gap-20'>
+        <div className='h-[92] overflow-hidden xl:hover:overflow-auto'>
+          <Sidebar />
+        </div>
+        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
+          <Component {...pageProps} />
+        </div>
+      </div>
+      
+    </div>
+  )
 }
 
 export default MyApp
