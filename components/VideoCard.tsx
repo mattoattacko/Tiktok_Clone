cc47fb44a8f593168f3958ed7f86c1c10c942233
unknown <matt.mcquain@gmail.com>
2022-07-22 18:11:09 -0700

    more work to VideoCard

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Video } from '../types'
 import { NextPage } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+import { BsFillPlayFill, BsFillPauseFill, BsPlay } from 'react-icons/bs'
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi'
+import { GoVerified } from 'react-icons/go'
 
 interface IProps {
   post: Video
@@ -12,7 +17,17 @@ interface IProps {
 //<IProps> is a prop and is passed to the component
 const VideoCard: NextPage<IProps> = ({ post }) => {
   return (
-    <div>VideoCard</div>
+    <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
+      <div>
+        <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded'>
+          <div>
+            <Link>
+              {/* we cant put an image as a child component of a Link, so we need to use a React fragment */}
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
