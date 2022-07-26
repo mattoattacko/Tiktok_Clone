84cef53c273f64535d4aec9ff41e9a1eefb7042f
unknown <matt.mcquain@gmail.com>
2022-07-25 20:10:09 -0700

    new Google oauth package installed to _app.tsx and .env. Using it in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image' //optimized image component
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { AiOutlineLogout } from 'react-icons/ai'
 import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
 
 import Logo from '../utils/tiktik-logo.png'
 
 const Navbar = () => {
+  const user = false;
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4" >
       <Link href='/'>
@@ -21,6 +24,22 @@ const Navbar = () => {
           />
         </div>        
       </Link>
+
+      <div>
+        Search
+      </div>
+      
+      <div>
+        {/* Check if there is a user logged in */}
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin 
+            onSuccess={(response) => console.log(response)} 
+            onError={() => console.log('error')}
+          />
+        )}
+      </div>
     </div>
   )
 }
