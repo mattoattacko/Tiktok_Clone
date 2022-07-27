66316d887d4044297415d743a792e840e9b49d5f
unknown <matt.mcquain@gmail.com>
2022-07-26 21:28:11 -0700

    fixed log-in styling for the sidebar and changed the VideoCard component to correctly route to the details page

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -39,41 +39,14 @@ const Sidebar = () => {
                 </span>
               </div>
             </Link>
-          </div>
-          {/* Need to figure out if the user is logged in or not, and based on that we can give them more options. So if no user profile exists, give them a button */}
-          { !userProfile && (
-            <div className="px-2 py-4 hidden xl:block">
-              <p className='text-gray-400'>
-                Log-in to like and comment on videos
-              </p>
-              <div className='pr-4'>
-                {/* This code can be found on the npm page for react-google-login */}
-                <GoogleLogin 
-                  clientId=''
-                  render={(renderProps) => (
-                    <button 
-                      className='cursor-pointer bg-white text-lg text-[#F51997] border-[1px] border-[#F51997] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#F51997]'
-                      onClick={renderProps.onClick}
-                      disabled={renderProps.disabled}
-                    >
-                      Log-in
-                    </button>
-                  )}
-                  onSuccess={() => {}}
-                  onFailure={() => {}}
-                  cookiePolicy='single_host_origin'
-                />
-              </div>
-            </div>
-          )}
+          </div>      
 
           <Discover />
           <SuggestedAccounts />
           <Footer />
         </div>
 
-      )}
-      
+      )}      
     </div>
   )
 }
---
+++ pages/detail/[id].tsx
@@ -0,0 +1,10 @@
+//url will be dynamic
+import React from 'react'
+
+const Detail = () => {
+  return (
+    <div>Detail</div>
+  )
+}
+
+export default Detail
