2d6170cd1083f66515f8c16a98a2999e10b94a74
unknown <matt.mcquain@gmail.com>
2022-07-29 19:19:51 -0700

    finished most of the NoResults component. Starting on Comments

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -9,6 +9,8 @@ import NoResults from './NoResults'
 const Comments = () => {
 
   const comments = []
+  const { userProfile } = useAuthStore()
+  const isPostingComment = false
 
   return (
     <div className='border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]'>
@@ -20,6 +22,24 @@ const Comments = () => {
           <NoResults text='Be the first to comment!' />
         )}
       </div>
+
+      {/* if the userProfile exists, then we show the comment form */}
+      {userProfile && (
+        <div className='absolute bottom-0 left-0 pb-6 px-2 md:px-10'>
+          <form className='flex gap-4' onSubmit={() => {}}>
+            <input 
+              value=''
+              onChange={() => {}}
+              placeholder='Add a comment...'
+              className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg'
+            />
+            {/* if currently posting a comment */}
+            <button className='text-md text-gray-400'>
+              {isPostingComment ? 'Commenting...' : 'Comment'}
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   )
 }
