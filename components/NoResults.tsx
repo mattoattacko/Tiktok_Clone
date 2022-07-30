eee2692f908a5b8fe2aa4c28a3eccf949ad061c6
unknown <matt.mcquain@gmail.com>
2022-07-29 18:01:50 -0700

    added the NoResults component and have it displaying correct dialog when no comment/video is available

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { MdOutlineVideocamOff } from 'react-icons/md'
+import { BiCommentX } from 'react-icons/bi'
 
 interface IProps {
   text: string;
@@ -6,7 +8,18 @@ interface IProps {
 
 const NoResults = ({ text }: IProps) => {
   return (
-    <div>NoResults</div>
+    <div className="flex flex-col justify-center items-center h-full w-full">
+      <p className='text-8xl'>
+        {/* if there are no comments yet, show no comments. Else show vid camera off icon. */}
+        {text === 'No Comments Yet' 
+          ? <BiCommentX />
+          : <MdOutlineVideocamOff />
+        }
+      </p>
+      <p className='text-2xl text-center'>
+        {text}
+      </p>
+    </div>
   )
 }
 
