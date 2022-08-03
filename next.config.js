3f42a8700f7497556a1e751a3c71601651d8bf8a
unknown <matt.mcquain@gmail.com>
2022-08-02 22:16:36 -0700

    small update on url

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 // In NextJs, we have to configure our images hostname. If we see the error that reads something like 'Invalid src prop xxx on 'next/images', hostname 'yt3.ggpht.com' is not configured under images in our next.config.js', that means that we need to come in here and add the domain. 
 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
